f05cccf6c08c4d8341bb7b871bf2667e9ab462bb
Akhil <akhilshaji957@gmail.com>
2023-07-11 23:14:34 +0530

    fb login

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,57 +1,85 @@
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useState } from "react";
 import {
   LoginSocialGoogle,
   LoginSocialFacebook,
   LoginSocialApple,
-} from 'reactjs-social-login';
+} from "reactjs-social-login";
 // CUSTOMIZE ANY UI BUTTON
 import {
   FacebookLoginButton,
   GoogleLoginButton,
   AppleLoginButton,
-} from 'react-social-login-buttons'
-import { User } from './User' 
+} from "react-social-login-buttons";
+import { User } from "./User";
 const REDIRECT_URI = window.location.href;
 function Login() {
-  const [provider, setProvider] = useState('')
-  const [profile, setProfile] = useState(null)
+  const [provider, setProvider] = useState("");
+  const [profile, setProfile] = useState(null);
 
   const onLoginStart = useCallback(() => {
-    alert('login start')
-  }, [])
+    alert("login start");
+  }, []);
 
   const onLogoutSuccess = useCallback(() => {
-    setProfile(null)
-    setProvider('')
-    alert('logout success')
-  }, [])
+    setProfile(null);
+    setProvider("");
+    alert("logout success");
+  }, []);
   return (
     <>
-       {provider && profile ? (
-        <User provider={provider} profile={profile} onLogout={onLogoutSuccess} />
+      {provider && profile ? (
+        <User
+          provider={provider}
+          profile={profile}
+          onLogout={onLogoutSuccess}
+        />
       ) : (
-        <div className={`App ${provider && profile ? 'hide' : ''}`}>
-          <h1 className='title'>ReactJS Social Login</h1>
-         
-
+        <div className={`App ${provider && profile ? "hide" : ""}`}>
+          <h1 className="title">ReactJS Social Login</h1>
           <LoginSocialGoogle
             isOnlyGetToken
-            client_id={process.env.REACT_APP_GG_APP_ID || ''}
+            client_id={process.env.REACT_APP_GG_APP_ID || ""}
             onLoginStart={onLoginStart}
             onResolve={({ provider, data }) => {
-              setProvider(provider)
-              setProfile(data)
+              setProvider(provider);
+              setProfile(data);
             }}
             onReject={(err) => {
-              console.log(err)
+              console.log(err);
             }}
           >
             {/* <h1>Google login</h1> */}
             <GoogleLoginButton />
           </LoginSocialGoogle>
-
-         
-        
+          <LoginSocialFacebook
+            isOnlyGetToken
+            client_id={process.env.REACT_APP_FB_APP_ID || ""}
+            onLoginStart={onLoginStart}
+            onResolve={({ provider, data }) => {
+              setProvider(provider);
+              setProfile(data);
+            }}
+            onReject={(err) => {
+              console.log(err);
+            }}
+          >
+            {/* <h1>Google login</h1> */}
+            <FacebookLoginButton />
+          </LoginSocialFacebook>
+          {/* <LoginSocialApple
+            isOnlyGetToken
+            client_id={process.env.REACT_APP_APPLE_ID || ""}
+            onLoginStart={onLoginStart}
+            onResolve={({ provider, data }) => {
+              setProvider(provider);
+              setProfile(data);
+            }}
+            onReject={(err) => {
+              console.log(err);
+            }}
+          >
+            <AppleLoginButton />
+          </LoginSocialApple> */}
         </div>
       )}
     </>
