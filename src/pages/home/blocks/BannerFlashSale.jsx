6cb586eb7e2d696c6b899bf76074640738a60877
vishnuprakash <vishnuprakash.jaishu@gmail.com>
2023-07-22 14:40:08 +0530

    timer

--- src/pages/home/blocks/BannerFlashSale.jsx
+++ src/pages/home/blocks/BannerFlashSale.jsx
@@ -1,7 +1,13 @@
 import "react-multi-carousel/lib/styles.css";
 import deviceImageRender from "../../../utils/deviceImageRender";
+import { useEffect, useState } from "react";
 
 function BannerFlashSale({ componentDatas }) {
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+
   const backgroundImage = {
     backgroundImage:
       `url("` +
@@ -11,6 +17,31 @@ function BannerFlashSale({ componentDatas }) {
       ) +
       `")`,
   };
+  const timeStart = componentDatas?.datas?.[0]?.start_time;
+  const timeEnd = componentDatas?.datas?.[0]?.end_time;
+  const startDate = new Date(timeStart);
+  startDate.setDate(startDate.getDate() - 1);
+  const endDate = new Date(timeEnd);
+  endDate.setDate(endDate.getDate() - 1);
+
+  const timer = async () => {
+    var msDiff = endDate - new Date();
+    console.log('msDiff',msDiff)
+    const seconds = Math.floor((msDiff / 1000) % 60);
+
+    const minutes = Math.floor((msDiff / 1000 / 60) % 60);
+
+    const hour = Math.floor((msDiff / 1000 / 60 / 60) % 24);
+    setHours(hour);
+    setMinutes(minutes);
+    setSeconds(seconds)
+    console.log('hour',hours)
+
+  };
+  useEffect(() => {
+    var myVar = setInterval(timer, 15000);
+    console.log("hour", hours);
+  }, []);
   return (
     <>
       <section className="banner-flashsale" style={backgroundImage}>
@@ -45,17 +76,17 @@ function BannerFlashSale({ componentDatas }) {
                 </div>
                 <div className="col-md-2">
                   <div className="timer-card">
-                    <h1>08</h1>
+                    <h1>{hours}</h1>
                   </div>
                 </div>
                 <div className="col-md-2">
                   <div className="timer-card">
-                    <h1>08</h1>
+                    <h1>{minutes}</h1>
                   </div>
                 </div>
                 <div className="col-md-2">
                   <div className="timer-card">
-                    <h1>08</h1>
+                    <h1>{seconds}</h1>
                   </div>
                 </div>
                 <h5 className="text-center">*valid on selected Items</h5>
