51a21928f54e33bc6370313a0838886b99fcc066
Bimal Raj S <bimalraj.s@gmail.com>
2023-07-22 15:14:42 +0530

    Deals banner image UI

--- src/pages/home/blocks/Deals.jsx
+++ src/pages/home/blocks/Deals.jsx
@@ -1,25 +1,20 @@
-
 import deviceImageRender from "../../../utils/deviceImageRender";
 function Deals({ componentDatas }) {
   const image = deviceImageRender(
-        componentDatas?.datas?.[0].desktop_image,
-        componentDatas?.datas?.[0].mobile_image
-      );
+    componentDatas?.datas?.[0].desktop_image,
+    componentDatas?.datas?.[0].mobile_image
+  );
 
   return (
     <>
       <div className="container-fluid cc-margin">
-          <div className="card deals-card" style={{height:150}}>
-            <div className="row align-items-center">
-              
-              {/* <div className="col-md-2"> */}
-                <img className="sticker" src={image} alt={componentDatas?.datas?.[0].image_alt} />
-              {/* </div> */}
-              
-            </div>
-          </div>
-
-        </div>
+        <img
+        className="deals-banner"
+          src={image}
+          alt={componentDatas?.datas?.[0].image_alt}
+        />
+        {/* </div> */}
+      </div>
     </>
   );
 }
