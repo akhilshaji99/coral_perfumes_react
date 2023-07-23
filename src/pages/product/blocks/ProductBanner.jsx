213f7f7d8557c7dac00a810ebef23650ac4ba7b2
vishnuprakash <vishnuprakash.jaishu@gmail.com>
2023-07-23 15:37:20 +0530

    product banner code change

--- src/pages/product/blocks/ProductBanner.jsx
+++ src/pages/product/blocks/ProductBanner.jsx
@@ -1,14 +1,21 @@
+import deviceImageRender from "../../../utils/deviceImageRender";
 
-// import deviceImageRender from "../../../utils/deviceImageRender";
-function ProductBanner({componentDatas}) {
-  console.log('productBanner',componentDatas)
-  return <>
-  <div className="card mb-4 bg-light border-0">
-          {/* card body */}
-          <img  src={
-                      componentDatas?.banner_image_url
-                    } className="img-fluid" alt="Coral Perfumes" />
-        </div>
-  </>;
+function ProductBanner({ componentDatas }) {
+  console.log("productBanner", componentDatas);
+  return (
+    <>
+      <div className="card mb-4 bg-light border-0">
+        {/* card body */}
+        <img
+          src={deviceImageRender(
+            componentDatas?.banner_image_url,
+            componentDatas?.banner_image_url
+          )}
+          className="img-fluid"
+          alt="Coral Perfumes"
+        />
+      </div>
+    </>
+  );
 }
 export default ProductBanner;
