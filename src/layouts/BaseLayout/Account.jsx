07bde2016dd7e64d2e8b9ce026bf91db2708ed35
Bimal Raj S <bimalraj.s@gmail.com>
2023-07-24 00:17:06 +0530

    Mini cart , Console error svg issue fix

--- src/layouts/BaseLayout/Account.jsx
+++ src/layouts/BaseLayout/Account.jsx
@@ -170,7 +170,7 @@ function Account() {
                     >
                       <li>
                         <NavLink to="/login">
-                          <a className="btn btn-dark btn-custom" >
+                          <a className="btn btn-dark btn-custom">
                             Sign In / Create Account
                           </a>
                         </NavLink>
@@ -340,10 +340,11 @@ function Account() {
                 </div>
                 <div className="list-inline-item">
                   <a
-                    href="#"
-                    id="dropdownMenuLink"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
+                    data-bs-toggle="offcanvas"
+                    data-bs-target="#offcanvasRight"
+                    href="#offcanvasExample"
+                    role="button"
+                    aria-controls="offcanvasRight"
                     className="text-center"
                   >
                     <svg
@@ -424,7 +425,7 @@ function Account() {
                         stroke="black"
                       />
                     </svg>
-                    <span className="dropdown-text">My Bag</span>
+                    <span className="dropdown-text">Find Store</span>
                   </a>
                 </div>
                 <div className="list-inline-item me-5">
@@ -471,6 +472,148 @@ function Account() {
               </div>
             </div>
           </div>
+          {/* Drawer */}
+          <div
+            className="offcanvas offcanvas-end"
+            tabIndex={-1}
+            id="offcanvasRight"
+            aria-labelledby="offcanvasRightLabel"
+          >
+            <div className="offcanvas-header border-bottom">
+              <div className="text-start ">
+                <h5 id="offcanvasRightLabel" className="mb-0 fs-4 text-white">
+                  <svg
+                    width="15"
+                    height="18"
+                    viewBox="0 0 15 18"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M4.75191 4.5H10.243C12.836 4.5 13.0953 5.78226 13.2707 7.34677L13.9571 13.3952C14.1783 15.379 13.5987 17 10.9294 17H4.07315C1.39624 17 0.816624 15.379 1.04542 13.3952L1.73181 7.34677C1.89959 5.78226 2.15889 4.5 4.75191 4.5Z"
+                      stroke="white"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                    <path
+                      d="M4.37036 6V3.08333C4.37036 1.83333 5.15277 1 6.32638 1H8.6736C9.84721 1 10.6296 1.83333 10.6296 3.08333V6"
+                      stroke="white"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>{" "}
+                  2 Items Added
+                </h5>
+              </div>
+              {/* <button
+                type="button"
+                className="btn-close text-reset"
+                data-bs-dismiss="offcanvas"
+                aria-label="Close"
+              /> */}
+            </div>
+            <div className="offcanvas-body">
+              <div className="">
+                <ul className="list-group list-group-flush">
+                  {/* list group */}
+                  <li className="list-group-item py-3 ps-0 border-top">
+                    {/* row */}
+                    <div className="row align-items-center">
+                      <div className="col-6 col-md-6 col-lg-7">
+                        <div className="d-flex">
+                          <img
+                            src="../assets/images/products/product-img-1.jpg"
+                            alt="Ecommerce"
+                            className="icon-shape icon-xxl"
+                          />
+                          <div className="ms-3">
+                            {/* title */}
+                            <a href="shop-single.html" className="text-inherit">
+                              <h6 className="mb-0">Haldiram's Sev Bhujia</h6>
+                            </a>
+                            <span>
+                              <small className="text-muted">.98 / lb</small>
+                            </span>
+                            {/* text */}
+                            <div className="mt-2 small lh-1">
+                              {" "}
+                              <a
+                                href="#!"
+                                className="text-decoration-none text-inherit"
+                              >
+                                {" "}
+                                <span className="me-1 align-text-bottom">
+                                  <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width={14}
+                                    height={14}
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    strokeWidth={2}
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    className="feather feather-trash-2 text-success"
+                                  >
+                                    <polyline points="3 6 5 6 21 6" />
+                                    <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+                                    <line x1={10} y1={11} x2={10} y2={17} />
+                                    <line x1={14} y1={11} x2={14} y2={17} />
+                                  </svg>
+                                </span>
+                                <span className="text-muted">Remove</span>
+                              </a>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      {/* input group */}
+                      <div className="col-4 col-md-3 col-lg-3">
+                        {/* input */}
+                        {/* input */}
+                        <div className="input-group input-spinner  ">
+                          <input
+                            type="button"
+                            defaultValue="-"
+                            className="button-minus  btn  btn-sm "
+                            data-field="quantity"
+                          />
+                          <input
+                            type="number"
+                            step={1}
+                            max={10}
+                            defaultValue={1}
+                            name="quantity"
+                            className="quantity-field form-control-sm form-input   "
+                          />
+                          <input
+                            type="button"
+                            defaultValue="+"
+                            className="button-plus btn btn-sm "
+                            data-field="quantity"
+                          />
+                        </div>
+                      </div>
+                      {/* price */}
+                      <div className="col-2 text-lg-end text-start text-md-end col-md-2">
+                        <span className="fw-bold">$5.00</span>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+                {/* btn */}
+                <div className="d-flex justify-content-between mt-4">
+                  <a href="#!" className="btn btn-primary">
+                    Continue Shopping
+                  </a>
+                  <a href="#!" className="btn btn-dark">
+                    Update Cart
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Drawer */}
         </div>
       </div>
     </>
---
+++ src/pages/shop/blocks/ShopMain.jsx
@@ -0,0 +1,275 @@
+import Sample from "../../../assets/img/sample-product.png";
+import TestBanner from "../../../assets/img/test-banner.png"
+
+
+function ShopMain() {
+  return (
+    <>
+      <section className="col-lg-9 col-md-12">
+        <div className="card mb-4 bg-light border-0">
+          {/* card body */}
+          <img src={TestBanner} className="img-fluid" alt="Coral Perfumes" />
+        </div>
+        <div className="row">
+          <div className="col-md-3">
+            <div className="product-grid">
+              <div className="card card-product product-box">
+                <div className="card-body">
+                  <span className="badge custom-badge">
+                    <svg
+                      width="29"
+                      height="25"
+                      viewBox="0 0 29 25"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M2.35118 15.597L11.6544 12.9424V22.8972C11.6544 25.22 13.2179 25.6901 15.1251 23.948L28.1316 12.0576C29.7295 10.6058 29.0595 9.40295 26.6368 9.40295L17.3336 12.0576V2.10278C17.3336 -0.220009 15.7701 -0.690096 13.8629 1.05199L0.856373 12.9424C-0.724342 14.408 -0.0542555 15.597 2.35118 15.597Z"
+                        fill="white"
+                      />
+                    </svg>{" "}
+                    20%
+                  </span>
+                  <span className="wishlist-button">
+                    <svg
+                      width="29"
+                      height="25"
+                      viewBox="0 0 29 25"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M27.5399 7.98659C27.5399 17.032 18.9036 22.3664 15.0342 23.642C14.5754 23.7967 13.8375 23.7967 13.3787 23.642C11.7233 23.1009 9.19022 21.8059 6.85662 19.8151C3.70527 17.1286 0.873047 13.1664 0.873047 7.98659C0.873047 3.98576 4.18396 0.75803 8.27274 0.75803C10.7061 0.75803 12.8601 1.89836 14.2164 3.63786C15.2167 2.3415 16.6397 1.41078 18.2545 0.996773C19.8693 0.582766 21.5811 0.709778 23.112 1.35719C25.7248 2.4782 27.5399 5.01012 27.5399 7.98659Z"
+                        stroke="black"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </svg>
+                  </span>
+                  <div className="text-center position-relative ">
+                    <a href="#!">
+                      {" "}
+                      <img
+                        src={Sample}
+                        alt="Coral Perfumes"
+                        className="mb-3 img-fluid product-img"
+                      />
+                    </a>
+                  </div>
+                  <button className="btn btn-dark w-100 mt-2">
+                    add to bag
+                  </button>
+                </div>
+              </div>
+              <h4>Coral Escape Pour Femme...</h4>
+              <div className="row custom-row1">
+                <div className="col-md-4 px-0">
+                  <h5 className="selling-price">AED 200</h5>
+                </div>
+                <div className="col-md-4 px-0">
+                  <h5 className="discounted-price">AED 400</h5>
+                </div>
+                <div className="col-md-4 px-0">
+                  <h5 className="discount-percentage">20% off</h5>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="product-grid">
+              <div className="card card-product product-box">
+                <div className="card-body">
+                  <span className="badge custom-badge">
+                    <svg
+                      width="29"
+                      height="25"
+                      viewBox="0 0 29 25"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M2.35118 15.597L11.6544 12.9424V22.8972C11.6544 25.22 13.2179 25.6901 15.1251 23.948L28.1316 12.0576C29.7295 10.6058 29.0595 9.40295 26.6368 9.40295L17.3336 12.0576V2.10278C17.3336 -0.220009 15.7701 -0.690096 13.8629 1.05199L0.856373 12.9424C-0.724342 14.408 -0.0542555 15.597 2.35118 15.597Z"
+                        fill="white"
+                      />
+                    </svg>{" "}
+                    20%
+                  </span>
+                  <span className="wishlist-button">
+                    <svg
+                      width="29"
+                      height="25"
+                      viewBox="0 0 29 25"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M27.5399 7.98659C27.5399 17.032 18.9036 22.3664 15.0342 23.642C14.5754 23.7967 13.8375 23.7967 13.3787 23.642C11.7233 23.1009 9.19022 21.8059 6.85662 19.8151C3.70527 17.1286 0.873047 13.1664 0.873047 7.98659C0.873047 3.98576 4.18396 0.75803 8.27274 0.75803C10.7061 0.75803 12.8601 1.89836 14.2164 3.63786C15.2167 2.3415 16.6397 1.41078 18.2545 0.996773C19.8693 0.582766 21.5811 0.709778 23.112 1.35719C25.7248 2.4782 27.5399 5.01012 27.5399 7.98659Z"
+                        stroke="black"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </svg>
+                  </span>
+                  <div className="text-center position-relative ">
+                    <a href="#!">
+                      {" "}
+                      <img
+                        src={Sample}
+                        alt="Coral Perfumes"
+                        className="mb-3 img-fluid product-img"
+                      />
+                    </a>
+                  </div>
+                  <button className="btn btn-dark w-100 mt-2">
+                    add to bag
+                  </button>
+                </div>
+              </div>
+              <h4>Coral Escape Pour Femme...</h4>
+              <div className="row custom-row1">
+                <div className="col-md-4 px-0">
+                  <h5 className="selling-price">AED 200</h5>
+                </div>
+                <div className="col-md-4 px-0">
+                  <h5 className="discounted-price">AED 400</h5>
+                </div>
+                <div className="col-md-4 px-0">
+                  <h5 className="discount-percentage">20% off</h5>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="product-grid">
+              <div className="card card-product product-box">
+                <div className="card-body">
+                  <span className="badge custom-badge">
+                    <svg
+                      width="29"
+                      height="25"
+                      viewBox="0 0 29 25"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M2.35118 15.597L11.6544 12.9424V22.8972C11.6544 25.22 13.2179 25.6901 15.1251 23.948L28.1316 12.0576C29.7295 10.6058 29.0595 9.40295 26.6368 9.40295L17.3336 12.0576V2.10278C17.3336 -0.220009 15.7701 -0.690096 13.8629 1.05199L0.856373 12.9424C-0.724342 14.408 -0.0542555 15.597 2.35118 15.597Z"
+                        fill="white"
+                      />
+                    </svg>{" "}
+                    20%
+                  </span>
+                  <span className="wishlist-button">
+                    <svg
+                      width="29"
+                      height="25"
+                      viewBox="0 0 29 25"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M27.5399 7.98659C27.5399 17.032 18.9036 22.3664 15.0342 23.642C14.5754 23.7967 13.8375 23.7967 13.3787 23.642C11.7233 23.1009 9.19022 21.8059 6.85662 19.8151C3.70527 17.1286 0.873047 13.1664 0.873047 7.98659C0.873047 3.98576 4.18396 0.75803 8.27274 0.75803C10.7061 0.75803 12.8601 1.89836 14.2164 3.63786C15.2167 2.3415 16.6397 1.41078 18.2545 0.996773C19.8693 0.582766 21.5811 0.709778 23.112 1.35719C25.7248 2.4782 27.5399 5.01012 27.5399 7.98659Z"
+                        stroke="black"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </svg>
+                  </span>
+                  <div className="text-center position-relative ">
+                    <a href="#!">
+                      {" "}
+                      <img
+                        src={Sample}
+                        alt="Coral Perfumes"
+                        className="mb-3 img-fluid product-img"
+                      />
+                    </a>
+                  </div>
+                  <button className="btn btn-dark w-100 mt-2">
+                    add to bag
+                  </button>
+                </div>
+              </div>
+              <h4>Coral Escape Pour Femme...</h4>
+              <div className="row custom-row1">
+                <div className="col-md-4 px-0">
+                  <h5 className="selling-price">AED 200</h5>
+                </div>
+                <div className="col-md-4 px-0">
+                  <h5 className="discounted-price">AED 400</h5>
+                </div>
+                <div className="col-md-4 px-0">
+                  <h5 className="discount-percentage">20% off</h5>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="product-grid">
+              <div className="card card-product product-box">
+                <div className="card-body">
+                  <span className="badge custom-badge">
+                    <svg
+                      width="29"
+                      height="25"
+                      viewBox="0 0 29 25"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M2.35118 15.597L11.6544 12.9424V22.8972C11.6544 25.22 13.2179 25.6901 15.1251 23.948L28.1316 12.0576C29.7295 10.6058 29.0595 9.40295 26.6368 9.40295L17.3336 12.0576V2.10278C17.3336 -0.220009 15.7701 -0.690096 13.8629 1.05199L0.856373 12.9424C-0.724342 14.408 -0.0542555 15.597 2.35118 15.597Z"
+                        fill="white"
+                      />
+                    </svg>{" "}
+                    20%
+                  </span>
+                  <span className="wishlist-button">
+                    <svg
+                      width="29"
+                      height="25"
+                      viewBox="0 0 29 25"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M27.5399 7.98659C27.5399 17.032 18.9036 22.3664 15.0342 23.642C14.5754 23.7967 13.8375 23.7967 13.3787 23.642C11.7233 23.1009 9.19022 21.8059 6.85662 19.8151C3.70527 17.1286 0.873047 13.1664 0.873047 7.98659C0.873047 3.98576 4.18396 0.75803 8.27274 0.75803C10.7061 0.75803 12.8601 1.89836 14.2164 3.63786C15.2167 2.3415 16.6397 1.41078 18.2545 0.996773C19.8693 0.582766 21.5811 0.709778 23.112 1.35719C25.7248 2.4782 27.5399 5.01012 27.5399 7.98659Z"
+                        stroke="black"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </svg>
+                  </span>
+                  <div className="text-center position-relative ">
+                    <a href="#!">
+                      {" "}
+                      <img
+                        src={Sample}
+                        alt="Coral Perfumes"
+                        className="mb-3 img-fluid product-img"
+                      />
+                    </a>
+                  </div>
+                  <button className="btn btn-dark w-100 mt-2">
+                    add to bag
+                  </button>
+                </div>
+              </div>
+              <h4>Coral Escape Pour Femme...</h4>
+              <div className="row custom-row1">
+                <div className="col-md-4 px-0">
+                  <h5 className="selling-price">AED 200</h5>
+                </div>
+                <div className="col-md-4 px-0">
+                  <h5 className="discounted-price">AED 400</h5>
+                </div>
+                <div className="col-md-4 px-0">
+                  <h5 className="discount-percentage">20% off</h5>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+export default ShopMain;
