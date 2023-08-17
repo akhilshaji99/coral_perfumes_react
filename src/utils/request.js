2fd085dc249d2c640944da354ea548bec5b7fe0c
Akhil <akhilshaji957@gmail.com>
2023-08-17 18:16:28 +0530

    token issue fix

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,42 +3,41 @@ import getUserToken from "./userToken";
 // import { formatAxiosError } from './error-handler';
 
 const service = axios.create({
-    baseURL: process.env.REACT_APP_API_URL // url = base url + request url
-    // timeout: 5000 // request timeout
+  baseURL: process.env.REACT_APP_API_URL, // url = base url + request url
+  // timeout: 5000 // request timeout
 });
 
 /// request interceptor do something before request is sent
 service.interceptors.request.use(
-    (config) => {
-      // const userData = JSON.parse(localStorage.getItem("userDatas"));
-      config.headers = {
-        // 'content-type': 'application/json',
-        // 'Access-Control-Allow-Origin': "*",
-        Authorization: 'Token ' + getUserToken() 
-      };
-      return config;
-    },
-    (error) => {
-      return Promise.reject(error);
+  (config) => {
+    // const userData = JSON.parse(localStorage.getItem("userDatas"));
+    config.headers = {
+      // 'content-type': 'application/json',
+      // 'Access-Control-Allow-Origin': "*",
+      Authorization: getUserToken() ? "Token " + getUserToken() : null,
+    };
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// interceptor response
+service.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error?.response?.status === 401) {
+      // window.location.href = `/login`;
+      return error?.response;
+    } else {
+      return error?.response;
     }
-  );
-  
-  // interceptor response
-  service.interceptors.response.use(
-    (response) => {
-      return response;
-    },
-    (error) => {
-      if (error?.response?.status === 401) {
-        // window.location.href = `/login`;
-        return error?.response
-        
-      }else{
-        return error?.response
-      }
     //   const formattedError = formatAxiosError(error.response.data);
-      return Promise.reject(error);
-    }
-  );
-  
-  export default service;
+    return Promise.reject(error);
+  }
+);
+
+export default service;
