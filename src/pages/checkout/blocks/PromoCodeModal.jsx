0b8d5238ccb7d9d530fcf390458e591b48350b23
Bimal Raj S <bimalraj.s@gmail.com>
2023-08-22 11:16:55 +0530

    Promo Code UI Modal

--- src/pages/checkout/blocks/PromoCodeModal.jsx
+++ src/pages/checkout/blocks/PromoCodeModal.jsx
@@ -35,64 +35,57 @@ function PromoCodeModal({ showPrmoCodeFlag, setShowPrmoCodeFlag }) {
   return (
     <>
       <div
-        className="modal fade"
+        className="modal fade promo-modal"
         id="promocodeModal"
         tabIndex={-1}
         aria-labelledby="promocodeModalLabel"
         aria-hidden="true"
       >
-        <div className="modal-dialog modal-dialog-centered">
-          <div className="modal-content p-4">
-            <div className="modal-header border-0">
-              <h5
-                className="modal-title fs-3 fw-bold "
-                id="promocodeModalLabel"
-              >
-                PROMO CODES
-              </h5>
-              <button
-                type="button"
-                className="btn-close"
-                // data-bs-dismiss="modal"
-                // aria-label="Close"
-                onClick={handleModalClose}
-              />
-            </div>
+        <div className="modal-dialog modal-dialog-centered ">
+          <div className="modal-content">
+            <h1 className="text-center " id="promocodeModalLabel">
+              PROMO CODES
+            </h1>
+            <button
+              type="button"
+              className="btn-close"
+              // data-bs-dismiss="modal"
+              // aria-label="Close"
+              onClick={handleModalClose}
+            />
             <div className="modal-body">
               {promoCodes?.map((code, index) => {
                 return (
                   <div className="row" key={index}>
                     {/* col */}
-                    <div className="col-lg-12 col-xxl-12 col-12 mb-4">
-                      {/* form */}
-                      <div className="card address-card">
-                        <div className="row align-items-center">
-                          <div className="col-md-12">
-                            <div className="card-body">
-                              <div className="row">
-                                <div className="col-md">
-                                  <p>CODE: {code.code}</p>
-                                </div>
-                                <div className="col-md px-0">
-                                  <button
-                                    class="btn btn-dark  validate w-100"
-                                    onClick={(e) => {
-                                      e.preventDefault();
-                                      applyPromoCode(code.id);
-                                    }}
-                                  >
-                                    Apply
-                                  </button>
-                                </div>
+                    {/* form */}
+                    <div className="card promo-card">
+                      <div className="row align-items-center">
+                        <div className="col-md-12">
+                          <div className="card-body">
+                            <div className="row align-items-center mb-4">
+                              <div className="col-md-8 col-8">
+                                <h2>CODE: {code.code}</h2>
                               </div>
-                              <div className="row">
-                                <p>{code.display_message}</p>
+                              <div className="col-md-4 col-4 px-0 text-end">
+                                <button
+                                  class="btn btn-dark  validate w-100"
+                                  onClick={(e) => {
+                                    e.preventDefault();
+                                    applyPromoCode(code.id);
+                                  }}
+                                >
+                                  Apply
+                                </button>
                               </div>
-                              <div className="row">
-                                <a href="" style={{ color: "black" }}>
-                                  Terms & Conditions
-                                </a>
-                              </div>
+                            </div>
+                            <div className="row">
+                              <p>{code.display_message}</p>
+                            </div>
+                            <div className="row">
+                              <a href="" style={{ color: "black" }}>
+                                Terms & Conditions
+                              </a>
                             </div>
                           </div>
                         </div>
