4825a025435675dea633397ac668425854262b7b
Akhil <akhilshaji957@gmail.com>
2023-10-14 20:07:29 +0530

    Slider swipe issue test 1

--- src/layouts/BaseLayout/Index.jsx
+++ src/layouts/BaseLayout/Index.jsx
@@ -56,7 +56,7 @@ const BaseLayout = () => {
         myAccountStatus={myAccountStatus}
         setMyAccountStatus={setMyAccountStatus}
       />
-      <Footer changeMobileMenuStatus={changeMobileMenuStatus} />
+      {/* <Footer changeMobileMenuStatus={changeMobileMenuStatus} /> */}
     </>
   );
 };
