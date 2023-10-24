772c7833929d89d5989e47bcd0d527bebba2ec05
Akhil <akhilshaji957@gmail.com>
2023-10-24 21:30:45 +0530

    Flash sale carosal - Iphone flickering fix

--- src/pages/home/blocks/FlashSale.jsx
+++ src/pages/home/blocks/FlashSale.jsx
@@ -1,12 +1,10 @@
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import AddToBag from "../../common/AddToBag";
 import CountdownTimer from "react-component-countdown-timer";
-import deviceImageRender from "../../../utils/deviceImageRender";
 import { useState } from "react";
-import { NavLink } from "react-router-dom";
-import WishlistIcon from "../../wishlist/blocks/WishlistIcon";
 import flashSale from "../../../assets/img/flash/flash-sale.gif";
+import FlashSaleSliderDatas from "../sliderDatas/FlashSaleSliderDatas";
+import Slider from "react-slick";
 
 function FlashSale({ componentDatas }) {
   const dynamicBackground = {
@@ -14,7 +12,6 @@ function FlashSale({ componentDatas }) {
   };
   //Code for timer
   const [falseSaleEnd, setFlashSaleEnd] = useState(false);
-  const [status, setStatus] = useState(false);
 
   const products = componentDatas?.datas?.[0]?.products;
   const timeEnd = componentDatas?.datas?.[0]?.end_time;
@@ -41,14 +38,27 @@ function FlashSale({ componentDatas }) {
   var msDiff = new Date(formattedDateTime) - new Date(); //Future date - current date
   counterValue = Math.floor(msDiff / 1000);
   //#End
+
+  var settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    initialSlide: 0.5,
+    responsive: [
+      {
+        breakpoint: 768, // Tablet and mobile view
+        settings: {
+          slidesToShow: 2.5,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
   return (
     <>
       {!falseSaleEnd ? (
         <div className="container-lg-fluid cc-margin">
-          <div
-            className="card flash-sale-container"
-            style={dynamicBackground}
-          >
+          <div className="card flash-sale-container" style={dynamicBackground}>
             <div className="d-block d-sm-none">
               <div className="row align-items-center mb-5 ">
                 <div className="col-6">
@@ -146,140 +156,77 @@ function FlashSale({ componentDatas }) {
                 </div>
               </div>
             </div>
-
-            <Carousel
-              additionalTransfrom={0}
-              autoPlay={false}
-              arrows
-              autoPlaySpeed={3500}
-              centerMode={false}
-              className=""
-              containerClass="container-with-dots"
-              dotListClass=""
-              draggable
-              focusOnSelect={false}
-              infinite={true}
-              itemClass=""
-              keyBoardControl
-              minimumTouchDrag={-10000}
-              pauseOnHover={false}
-              renderArrowsWhenDisabled={false}
-              renderButtonGroupOutside={false}
-              renderDotsOutside={false}
-              responsive={{
-                desktop: {
-                  breakpoint: {
-                    max: 3000,
-                    min: 1024,
+            <div className="desktop_slider">
+              <Carousel
+                additionalTransfrom={0}
+                autoPlay={false}
+                arrows
+                autoPlaySpeed={3500}
+                centerMode={false}
+                className=""
+                containerClass="container-with-dots"
+                dotListClass=""
+                draggable
+                focusOnSelect={false}
+                infinite={true}
+                itemClass=""
+                keyBoardControl
+                minimumTouchDrag={-10000}
+                pauseOnHover={false}
+                renderArrowsWhenDisabled={false}
+                renderButtonGroupOutside={false}
+                renderDotsOutside={false}
+                responsive={{
+                  desktop: {
+                    breakpoint: {
+                      max: 3000,
+                      min: 1024,
+                    },
+                    items: 4,
+                    partialVisibilityGutter: 40,
                   },
-                  items: 4,
-                  partialVisibilityGutter: 40,
-                },
-                mobile: {
-                  breakpoint: {
-                    max: 464,
-                    min: 0,
+                  mobile: {
+                    breakpoint: {
+                      max: 464,
+                      min: 0,
+                    },
+                    items: 2.5,
+                    partialVisibilityGutter: 20,
                   },
-                  items: 2.5,
-                  partialVisibilityGutter: 20,
-                },
-                tablet: {
-                  breakpoint: {
-                    max: 1024,
-                    min: 464,
+                  tablet: {
+                    breakpoint: {
+                      max: 1024,
+                      min: 464,
+                    },
+                    items: 3,
+                    partialVisibilityGutter: 30,
                   },
-                  items: 3,
-                  partialVisibilityGutter: 30,
-                },
-              }}
-              rewind={false}
-              rewindWithAnimation={false}
-              rtl={false}
-              shouldResetAutoplay
-              showDots={false}
-              sliderClass=""
-              slidesToSlide={2}
-              swipeable
-            >
-              {products.map((product, index) => {
-                return (
-                  <div className="product-grid" key={index}>
-                    <div className="card card-product  carousel-product-margin">
-                      <div className="card-body product-box">
-                        {product.discount_percentage ? (
-                          <span className="badge custom-badge">
-                            <svg
-                              width="29"
-                              height="25"
-                              viewBox="0 0 29 25"
-                              fill="none"
-                              xmlns="http://www.w3.org/2000/svg"
-                            >
-                              <path
-                                d="M2.35118 15.597L11.6544 12.9424V22.8972C11.6544 25.22 13.2179 25.6901 15.1251 23.948L28.1316 12.0576C29.7295 10.6058 29.0595 9.40295 26.6368 9.40295L17.3336 12.0576V2.10278C17.3336 -0.220009 15.7701 -0.690096 13.8629 1.05199L0.856373 12.9424C-0.724342 14.408 -0.0542555 15.597 2.35118 15.597Z"
-                                fill="white"
-                              />
-                            </svg>{" "}
-                            {product.discount_percentage}%
-                          </span>
-                        ) : null}
-                        <WishlistIcon
-                          product_slug={product?.slug}
-                          is_in_wishlist={product?.is_in_wishlist}
-                          changeWishlistStatus={() => {
-                            product.is_in_wishlist = !product?.is_in_wishlist;
-                            setStatus(!status);
-                          }}
-                        />
-                        <div className="text-center position-relative ">
-                          <a href="#!">
-                            {" "}
-                            <div className="product-img">
-                              <NavLink
-                                to={`/product-details/?slug=${product?.slug}`}
-                              >
-                                <img
-                                  src={deviceImageRender(
-                                    product?.product_listing_image
-                                  )}
-                                  alt="Coral Perfumes"
-                                  className=" img-fluid "
-                                />
-                              </NavLink>
-                            </div>
-                          </a>
-                        </div>
-                        <AddToBag variant_id={product?.id} />
-                      </div>
-                      <div className="card-footer">
-                        <h4 className="ellipsis-text">{product.name}</h4>
-                        <div className="row custom-row1">
-                          <div className="col-md-4 col-6 px-0">
-                            <h5 className="selling-price">
-                              {product?.currency_code} {product.price_amount}
-                            </h5>
-                          </div>
-                          {product.original_amount ? (
-                            <div className="col-md-4 col-6 px-0">
-                              <h5 className="discounted-price">
-                                {product?.currency_code} {product.original_amount}
-                              </h5>
-                            </div>
-                          ) : null}
-                          {product.discount_percentage ? (
-                            <div className="col-md-4 px-0">
-                              <h5 className="discount-percentage">
-                                {product.discount_percentage}% off
-                              </h5>
-                            </div>
-                          ) : null}
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                );
-              })}
-            </Carousel>
+                }}
+                rewind={false}
+                rewindWithAnimation={false}
+                rtl={false}
+                shouldResetAutoplay
+                showDots={false}
+                sliderClass=""
+                slidesToSlide={2}
+                swipeable
+              >
+                {products.map((product, index) => {
+                  return (
+                    <FlashSaleSliderDatas product={product} index={index} />
+                  );
+                })}
+              </Carousel>
+            </div>
+            <div className="mob_tab_slider">
+              <Slider {...settings} className="prod-carosal-mob-padding">
+                {products.map((product, index) => {
+                  return (
+                    <FlashSaleSliderDatas product={product} index={index} />
+                  );
+                })}
+              </Slider>
+            </div>
           </div>
         </div>
       ) : null}
