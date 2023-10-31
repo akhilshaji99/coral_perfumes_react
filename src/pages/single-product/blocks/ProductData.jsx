6de9845f3b97c9e1d2d0533a120c10e6fb155c43
Akhil <akhilshaji957@gmail.com>
2023-10-31 20:56:44 +0530

    Orders arrow enable

--- src/pages/single-product/blocks/ProductData.jsx
+++ src/pages/single-product/blocks/ProductData.jsx
@@ -6,82 +6,6 @@ function productData({ productDatas, currentVariant }) {
       <div className="row py-5">
         <div className="col-xl-9 col-sm-12">
           <div className="info-box">
-            {productDatas?.message_1 ? (
-              <h2>
-                <span>
-                  <svg
-                    width={20}
-                    height={20}
-                    viewBox="0 0 20 20"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M1 1.07776H2.60222C3.5967 1.07776 4.37939 1.91476 4.29652 2.87776L3.53224 11.8418C3.5024 12.189 3.54668 12.5386 3.66229 12.8683C3.7779 13.1979 3.9623 13.5005 4.20383 13.7569C4.44535 14.0133 4.73872 14.2178 5.06535 14.3576C5.39198 14.4973 5.74476 14.5692 6.10131 14.5688H15.908C17.234 14.5688 18.3942 13.5068 18.4955 12.2198L18.9927 5.46976C19.1032 3.97576 17.943 2.76076 16.4052 2.76076H4.51751M7.4457 6.47776H18.4955M14.1216 19.0778C14.4269 19.0778 14.7196 18.9592 14.9355 18.7483C15.1514 18.5373 15.2726 18.2511 15.2726 17.9528C15.2726 17.6544 15.1514 17.3682 14.9355 17.1573C14.7196 16.9463 14.4269 16.8278 14.1216 16.8278C13.8163 16.8278 13.5236 16.9463 13.3077 17.1573C13.0919 17.3682 12.9706 17.6544 12.9706 17.9528C12.9706 18.2511 13.0919 18.5373 13.3077 18.7483C13.5236 18.9592 13.8163 19.0778 14.1216 19.0778ZM6.75509 19.0778C7.06036 19.0778 7.35313 18.9592 7.56898 18.7483C7.78484 18.5373 7.90611 18.2511 7.90611 17.9528C7.90611 17.6544 7.78484 17.3682 7.56898 17.1573C7.35313 16.9463 7.06036 16.8278 6.75509 16.8278C6.44982 16.8278 6.15706 16.9463 5.9412 17.1573C5.72534 17.3682 5.60407 17.6544 5.60407 17.9528C5.60407 18.2511 5.72534 18.5373 5.9412 18.7483C6.15706 18.9592 6.44982 19.0778 6.75509 19.0778Z"
-                      stroke="black"
-                      strokeMiterlimit={10}
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                  </svg>
-                </span>
-                {productDatas?.message_1}
-              </h2>
-            ) : null}
-            {productDatas?.message_2 ? (
-              <h2>
-                <span>
-                  <svg
-                    width={20}
-                    height={20}
-                    viewBox="0 0 20 20"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M13.375 7.37776H6.625M13.375 12.7778H6.625M7.3 19.0778H12.7C17.2 19.0778 19 17.2778 19 12.7778V7.37776C19 2.87776 17.2 1.07776 12.7 1.07776H7.3C2.8 1.07776 1 2.87776 1 7.37776V12.7778C1 17.2778 2.8 19.0778 7.3 19.0778Z"
-                      stroke="black"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                  </svg>
-                </span>
-                {productDatas?.message_2}
-              </h2>
-            ) : null}
-          </div>
-        </div>
-      </div>
-      {productDatas?.tabby_attribute_value === "1" ? (
-        <div className="row py-2 align-items-center payment-tabby">
-          <div className="col-xl-2 col-sm-2 col-2">
-            <img src={Tabby} alt="Coral Perfumes" />
-          </div>
-          <div
-            className="col-xl-7 col-sm-7 col-8 "
-            style={{ marginLeft: "3%" }}
-          >
-            {productDatas?.tabby_text}
-          </div>
-        </div>
-      ) : null}
-      {productDatas?.tamara_attribute_value === "1" && currentVariant ? (
-        <div className="row py-2 align-items-center payment-tabby">
-          <div className="col-xl-9 col-sm-12 col-12">
-            <tamara-widget
-              type="tamara-summary"
-              amount={currentVariant?.price_amount}
-              inline-type="2"
-            ></tamara-widget>
-          </div>
-          {/* <div className="col-xl-11 col-sm-10 col-10 text-sm-end text-lg-start">
-            <span>4 interest-free payments AED 40 </span>
-          </div> */}
-        </div>
-      ) : null}
-      <div className="row py-5">
-        <div className="col-xl-9 col-sm-12">
-          <div className="info-box">
             {productDatas?.order_delivery_message ? (
               <h2>
                 <span>
@@ -131,6 +55,82 @@ function productData({ productDatas, currentVariant }) {
                   </svg>
                 </span>
                 {productDatas?.emirate_message}
+              </h2>
+            ) : null}
+          </div>
+        </div>
+      </div>
+      {productDatas?.tabby_attribute_value === "1" ? (
+        <div className="row py-2 align-items-center payment-tabby">
+          <div className="col-xl-2 col-sm-2 col-2">
+            <img src={Tabby} alt="Coral Perfumes" />
+          </div>
+          <div
+            className="col-xl-7 col-sm-7 col-8 "
+            style={{ marginLeft: "3%" }}
+          >
+            {productDatas?.tabby_text}
+          </div>
+        </div>
+      ) : null}
+      {productDatas?.tamara_attribute_value === "1" && currentVariant ? (
+        <div className="row py-2 align-items-center payment-tabby">
+          <div className="col-xl-9 col-sm-12 col-12">
+            <tamara-widget
+              type="tamara-summary"
+              amount={currentVariant?.price_amount}
+              inline-type="2"
+            ></tamara-widget>
+          </div>
+          {/* <div className="col-xl-11 col-sm-10 col-10 text-sm-end text-lg-start">
+            <span>4 interest-free payments AED 40 </span>
+          </div> */}
+        </div>
+      ) : null}
+      <div className="row py-5">
+        <div className="col-xl-9 col-sm-12">
+          <div className="info-box">
+            {productDatas?.message_1 ? (
+              <h2>
+                <span>
+                  <svg
+                    width={20}
+                    height={20}
+                    viewBox="0 0 20 20"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M1 1.07776H2.60222C3.5967 1.07776 4.37939 1.91476 4.29652 2.87776L3.53224 11.8418C3.5024 12.189 3.54668 12.5386 3.66229 12.8683C3.7779 13.1979 3.9623 13.5005 4.20383 13.7569C4.44535 14.0133 4.73872 14.2178 5.06535 14.3576C5.39198 14.4973 5.74476 14.5692 6.10131 14.5688H15.908C17.234 14.5688 18.3942 13.5068 18.4955 12.2198L18.9927 5.46976C19.1032 3.97576 17.943 2.76076 16.4052 2.76076H4.51751M7.4457 6.47776H18.4955M14.1216 19.0778C14.4269 19.0778 14.7196 18.9592 14.9355 18.7483C15.1514 18.5373 15.2726 18.2511 15.2726 17.9528C15.2726 17.6544 15.1514 17.3682 14.9355 17.1573C14.7196 16.9463 14.4269 16.8278 14.1216 16.8278C13.8163 16.8278 13.5236 16.9463 13.3077 17.1573C13.0919 17.3682 12.9706 17.6544 12.9706 17.9528C12.9706 18.2511 13.0919 18.5373 13.3077 18.7483C13.5236 18.9592 13.8163 19.0778 14.1216 19.0778ZM6.75509 19.0778C7.06036 19.0778 7.35313 18.9592 7.56898 18.7483C7.78484 18.5373 7.90611 18.2511 7.90611 17.9528C7.90611 17.6544 7.78484 17.3682 7.56898 17.1573C7.35313 16.9463 7.06036 16.8278 6.75509 16.8278C6.44982 16.8278 6.15706 16.9463 5.9412 17.1573C5.72534 17.3682 5.60407 17.6544 5.60407 17.9528C5.60407 18.2511 5.72534 18.5373 5.9412 18.7483C6.15706 18.9592 6.44982 19.0778 6.75509 19.0778Z"
+                      stroke="black"
+                      strokeMiterlimit={10}
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                </span>
+                {productDatas?.message_1}
+              </h2>
+            ) : null}
+            {productDatas?.message_2 ? (
+              <h2>
+                <span>
+                  <svg
+                    width={20}
+                    height={20}
+                    viewBox="0 0 20 20"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M13.375 7.37776H6.625M13.375 12.7778H6.625M7.3 19.0778H12.7C17.2 19.0778 19 17.2778 19 12.7778V7.37776C19 2.87776 17.2 1.07776 12.7 1.07776H7.3C2.8 1.07776 1 2.87776 1 7.37776V12.7778C1 17.2778 2.8 19.0778 7.3 19.0778Z"
+                      stroke="black"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                </span>
+                {productDatas?.message_2}
               </h2>
             ) : null}
           </div>
