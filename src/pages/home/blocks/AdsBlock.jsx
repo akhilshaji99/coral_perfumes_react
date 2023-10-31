59698f173ec97259cf51d695d4e4b63768135d1f
Akhil <akhilshaji957@gmail.com>
2023-10-31 21:06:13 +0530

    ads block new section code

--- src/pages/home/blocks/AdsBlock.jsx
+++ src/pages/home/blocks/AdsBlock.jsx
@@ -1,9 +1,80 @@
 import { Link } from "react-router-dom";
 import deviceImageRender from "../../../utils/deviceImageRender";
+import "../../../assets/css/adsblock.css";
 function AdsBlock({ componentDatas }) {
   return (
     <>
-      <div className="container-fluid full-width cc-margin">
+      <section className="collect-banner">
+        {/*--lg-view*/}
+        <div className="container-fluid sm-none">
+          <div className="row collection-img">
+            <div className="col-md-6">
+              <a href="#">
+                {" "}
+                <img
+                  src="https://coralecom.cloud6.ae/media/components/Offer_tiles_Banner_Exclusive_WEBP.webp"
+                  alt="perfume-bottle"
+                />
+              </a>
+            </div>
+            <div className="col-md-6">
+              <a href="#">
+                {" "}
+                <img
+                  src="https://coralecom.cloud6.ae/media/components/Offer_tiles_Banner_Exclusive_WEBP.webp"
+                  alt="perfume-bottle-pink"
+                />
+              </a>
+            </div>
+            <div className="col-md-6 pt-7">
+              <a href="#">
+                {" "}
+                <img
+                  src="https://coralecom.cloud6.ae/media/components/Offer_tiles_Banner_Exclusive_WEBP.webp"
+                  alt="perfume-bottle"
+                />
+              </a>
+            </div>
+            <div className="col-md-6 pt-7">
+              <a href="#">
+                {" "}
+                <img src="https://coralecom.cloud6.ae/media/components/Offer_tiles_Banner_Exclusive_WEBP.webp" alt="perfume-bottle-pink" />
+              </a>
+            </div>
+          </div>
+        </div>
+        {/*--sm-view*/}
+        <div className="lg-none">
+          <div className="mob-inspired">
+            <a href="#">
+              <img
+                src="https://coralecom.cloud6.ae/media/components/Offer_tiles_Banner_Exclusive_WEBP.webp"
+                alt="perfume-bottle"
+              />
+            </a>
+          </div>
+          <div className="mob-collect-banner">
+            <a href="#">
+              {" "}
+              <img src="https://coralecom.cloud6.ae/media/components/Offer_tiles_Banner_Exclusive_WEBP.webp" alt="perfume-bottle-pink" />
+            </a>
+          </div>
+          <div className="mob-inspired">
+            <a href="#">
+              {" "}
+              <img src="https://coralecom.cloud6.ae/media/components/Offer_tiles_Banner_Exclusive_WEBP.webp" alt="perfume-bottle" />
+            </a>
+          </div>
+          <div className="mob-collect-banner">
+            <a href="#">
+              {" "}
+              <img src="https://coralecom.cloud6.ae/media/components/Offer_tiles_Banner_Exclusive_WEBP.webp" alt="Oil Collections" />
+            </a>
+          </div>
+        </div>
+      </section>
+
+      {/* <div className="container-fluid full-width cc-margin">
         <div className="row g-8 ads-block justify-content-center">
           {componentDatas?.datas?.map((adBlock, index) => {
             return (
@@ -27,7 +98,7 @@ function AdsBlock({ componentDatas }) {
             );
           })}
         </div>
-      </div>
+      </div> */}
     </>
   );
 }
