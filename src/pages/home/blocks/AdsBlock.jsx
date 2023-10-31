d5807ed63791e4d6292b53bd58409e6a9e883df4
Akhil <akhilshaji957@gmail.com>
2023-10-31 21:28:36 +0530

    Ads block dynamically loaded

--- src/pages/home/blocks/AdsBlock.jsx
+++ src/pages/home/blocks/AdsBlock.jsx
@@ -8,97 +8,51 @@ function AdsBlock({ componentDatas }) {
         {/*--lg-view*/}
         <div className="container-fluid sm-none">
           <div className="row collection-img">
-            <div className="col-md-6">
-              <a href="#">
-                {" "}
-                <img
-                  src="https://coralecom.cloud6.ae/media/components/Offer_tiles_Banner_Exclusive_WEBP.webp"
-                  alt="perfume-bottle"
-                />
-              </a>
-            </div>
-            <div className="col-md-6">
-              <a href="#">
-                {" "}
-                <img
-                  src="https://coralecom.cloud6.ae/media/components/Offer_tiles_Banner_Exclusive_WEBP.webp"
-                  alt="perfume-bottle-pink"
-                />
-              </a>
-            </div>
-            <div className="col-md-6 pt-7">
-              <a href="#">
-                {" "}
-                <img
-                  src="https://coralecom.cloud6.ae/media/components/Offer_tiles_Banner_Exclusive_WEBP.webp"
-                  alt="perfume-bottle"
-                />
-              </a>
-            </div>
-            <div className="col-md-6 pt-7">
-              <a href="#">
-                {" "}
-                <img src="https://coralecom.cloud6.ae/media/components/Offer_tiles_Banner_Exclusive_WEBP.webp" alt="perfume-bottle-pink" />
-              </a>
-            </div>
+            {componentDatas?.datas?.map((adBlock, index) => {
+              return (
+                <div
+                  className={`col-md-6 ${
+                    index === 2 || index === 3 ? `pt-7` : ""
+                  }`}
+                  key={index}
+                >
+                  <a href="#">
+                    <img
+                      src={deviceImageRender(
+                        adBlock?.desktop_image,
+                        adBlock?.mobile_image
+                      )}
+                      alt={adBlock?.image_alt}
+                    />
+                  </a>
+                </div>
+              );
+            })}
           </div>
         </div>
         {/*--sm-view*/}
         <div className="lg-none">
-          <div className="mob-inspired">
-            <a href="#">
-              <img
-                src="https://coralecom.cloud6.ae/media/components/Offer_tiles_Banner_Exclusive_WEBP.webp"
-                alt="perfume-bottle"
-              />
-            </a>
-          </div>
-          <div className="mob-collect-banner">
-            <a href="#">
-              {" "}
-              <img src="https://coralecom.cloud6.ae/media/components/Offer_tiles_Banner_Exclusive_WEBP.webp" alt="perfume-bottle-pink" />
-            </a>
-          </div>
-          <div className="mob-inspired">
-            <a href="#">
-              {" "}
-              <img src="https://coralecom.cloud6.ae/media/components/Offer_tiles_Banner_Exclusive_WEBP.webp" alt="perfume-bottle" />
-            </a>
-          </div>
-          <div className="mob-collect-banner">
-            <a href="#">
-              {" "}
-              <img src="https://coralecom.cloud6.ae/media/components/Offer_tiles_Banner_Exclusive_WEBP.webp" alt="Oil Collections" />
-            </a>
-          </div>
-        </div>
-      </section>
-
-      {/* <div className="container-fluid full-width cc-margin">
-        <div className="row g-8 ads-block justify-content-center">
           {componentDatas?.datas?.map((adBlock, index) => {
             return (
               <div
-                className={`col-md-6 col-12 ${
-                  index % 2 === 0 ? `side-padding` : ""
+                className={`${
+                  index % 2 === 0 ? `mob-inspired` : "mob-collect-banner"
                 }`}
-                key={index}
               >
-                <Link to={adBlock?.link}>
+                <a href="#">
                   <img
                     src={deviceImageRender(
                       adBlock?.desktop_image,
                       adBlock?.mobile_image
                     )}
-                    className="img-fluid"
                     alt={adBlock?.image_alt}
                   />
-                </Link>
+                </a>
               </div>
             );
           })}
         </div>
-      </div> */}
+      </section>
     </>
   );
 }
