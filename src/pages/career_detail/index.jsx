536819df45d8448f153cb0f709b082d46a31c87c
Akhil <akhilshaji957@gmail.com>
2023-11-28 20:20:23 +0530

    Meta logo change

--- src/pages/career_detail/index.jsx
+++ src/pages/career_detail/index.jsx
@@ -7,15 +7,14 @@ import { useState, useRef, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import request from "../../utils/request";
 import deviceImageRender from "../../utils/deviceImageRender";
-
-
+import BreadCrumps from "../common/BreadCrumps";
 
 function Index() {
   const [banners, setBanners] = useState({});
   const [jobDetails, setJobDetails] = useState({});
 
   const urlParams = useParams([]);
-  console.log("urlParams",urlParams?.id)
+  console.log("urlParams", urlParams?.id);
   useEffect(() => {
     getCareerDetails();
   }, []);
@@ -24,7 +23,7 @@ function Index() {
       //   var bodyFormData = new FormData();
       //   bodyFormData.append("token", getUserToken());
       //   dispatch(changeApiCallStatus(false)); // Change api call status
-      const response = await request.get("job-detail/"+urlParams?.id+"/");
+      const response = await request.get("job-detail/" + urlParams?.id + "/");
       if (response.data) {
         setBanners(response.data.banner_image);
         setJobDetails(response.data.job_details);
@@ -38,34 +37,32 @@ function Index() {
   };
   return (
     <>
-   <div className="page-new">
+      <div className="page-new">
+        <BreadCrumps />
         <div className="container-new">
-            <div className="title-page">
-                <h1>Careers</h1>
-            </div>
-            <div className="banner-careers">
-            {
-            banners?.banner_image ?
-            <img
-              src={deviceImageRender(banners?.banner_image)}
-              alt="img-banner"
-            />:null
-            }
-            </div>
+          <div className="title-page">
+            <h1>Careers</h1>
+          </div>
+          <div className="banner-careers">
+            {banners?.banner_image ? (
+              <img
+                src={deviceImageRender(banners?.banner_image)}
+                alt="img-banner"
+              />
+            ) : null}
+          </div>
         </div>
         <section>
-            <div className="container-new">
-                <div className="retail-sales">
-                    <h2>{jobDetails?.job_title}</h2>
-                    <span>{jobDetails?.last_date}</span>
-                    {/* <h3>{jobDetails?.last_date}</h3> */}
-                    <p>
-                    {jobDetails?.job_description}
-                    </p>
-                </div>
-                <div className="respons-list">
-                    <h3>Responsibilities</h3>
-                    {/* <ul>
+          <div className="container-new">
+            <div className="retail-sales">
+              <h2>{jobDetails?.job_title}</h2>
+              <span>{jobDetails?.last_date}</span>
+              {/* <h3>{jobDetails?.last_date}</h3> */}
+              <p>{jobDetails?.job_description}</p>
+            </div>
+            <div className="respons-list">
+              <h3>Responsibilities</h3>
+              {/* <ul>
                         <li>Meet and assist customers in a friendly and professional manner.</li>
                         <li>Provide product information and recommendations to customers.</li>
                         <li>Assist in maintaining the visual appearance of the store, including displays, shelves, and product arrangements.</li>
@@ -77,48 +74,80 @@ function Index() {
 
                     
                     </ul> */}
-                      <div   dangerouslySetInnerHTML={{
-                        __html: jobDetails?.job_responsibilities,
-                      }}>
-                      
-                        </div>
-                </div>
-                <div className="apply-form">
-                 
-                    <form action="#" method="post">
-                        <h3>Apply for this position</h3>
-                        <div className="form-group">
-                            <input type="text" id="first_name" name="first_name" placeholder="First Name" required/>
-                            <input type="text" id="last_name" name="last_name" placeholder="Last Name" required/>
-                        </div>
-                        <div className="form-group">
-                            <input type="text" id="E-mail*" name="E-mail" placeholder="E-mail*"/>
-                            <input type="text" id="num" name="num" placeholder="055 923 8088"/>
-                        </div>
-                        <div className="form-group file-upload">
-                            <label for="file_input">
-                                <img src={upload1} alt="img"/>
-                                <div className="upload-top">
-                                    <h4>Upload your Resume</h4>
-                                    <p>OR</p>
-                                     <p>Drag and drop your Resume here</p></div>
-                            </label>
-                            <input type="file" id="file_input" name="file_upload" accept="image/*"/>
-                        </div>
-                       
-                        <div className="form-group-2 lt-count">
-                            <label>Add a cover letter*</label>
-                            <input type="text" id="field1" name="field1" placeholder="Add a cover letter"/>
-                            <span id="word-count">0/200</span>
-                        </div>
-                        <div className="form-group">
-                            <input type="submit" value="Apply"/>
-                        </div>
-                    </form>
-                </div>
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: jobDetails?.job_responsibilities,
+                }}
+              ></div>
             </div>
+            <div className="apply-form">
+              <form action="#" method="post">
+                <h3>Apply for this position</h3>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    id="first_name"
+                    name="first_name"
+                    placeholder="First Name"
+                    required
+                  />
+                  <input
+                    type="text"
+                    id="last_name"
+                    name="last_name"
+                    placeholder="Last Name"
+                    required
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    id="E-mail*"
+                    name="E-mail"
+                    placeholder="E-mail*"
+                  />
+                  <input
+                    type="text"
+                    id="num"
+                    name="num"
+                    placeholder="055 923 8088"
+                  />
+                </div>
+                <div className="form-group file-upload">
+                  <label for="file_input">
+                    <img src={upload1} alt="img" />
+                    <div className="upload-top">
+                      <h4>Upload your Resume</h4>
+                      <p>OR</p>
+                      <p>Drag and drop your Resume here</p>
+                    </div>
+                  </label>
+                  <input
+                    type="file"
+                    id="file_input"
+                    name="file_upload"
+                    accept="image/*"
+                  />
+                </div>
+
+                <div className="form-group-2 lt-count">
+                  <label>Add a cover letter*</label>
+                  <input
+                    type="text"
+                    id="field1"
+                    name="field1"
+                    placeholder="Add a cover letter"
+                  />
+                  <span id="word-count">0/200</span>
+                </div>
+                <div className="form-group">
+                  <input type="submit" value="Apply" />
+                </div>
+              </form>
+            </div>
+          </div>
         </section>
-    </div>
+      </div>
     </>
   );
 }
