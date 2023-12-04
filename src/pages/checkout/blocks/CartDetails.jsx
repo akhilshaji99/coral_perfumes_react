db729596d0e6bad7f80e7a5087f10970be86cb21
Akhil <akhilshaji957@gmail.com>
2023-12-04 22:51:18 +0530

    Checkout Quantity

--- src/pages/checkout/blocks/CartDetails.jsx
+++ src/pages/checkout/blocks/CartDetails.jsx
@@ -28,8 +28,8 @@ function CartDetails({ cartDatas ,scrollToPaymentComponent}) {
                         {cartData?.variant?.original_amount ? (
                           <span>AED {cartData?.variant?.original_amount}</span>
                         ) : null}
-                         &nbsp;<span style={{textDecoration:'none'}}>Qty:</span> {cartData?.quantity}
                       </h6>
+                      <span style={{textDecoration:'none'}}>Qty:</span> {cartData?.quantity}
                     </div>
                   </div>
                 </li>
