6cdf2472d911d1a3f7bafc2addb289e848fb6b9c
Akhil <akhilshaji957@gmail.com>
2024-04-19 23:49:02 +0530

    Slug SEO Change

--- src/components/RedirectComponent.jsx
+++ src/components/RedirectComponent.jsx
@@ -1,12 +1,11 @@
 import React, { useEffect } from "react";
 
 function RedirectComponent(props) {
-    
-  const redirectUrl=props.redirectUrl;
+  const redirectUrl = props.redirectUrl;
 
   const url = window.location.pathname;
   const splittedUrl = url.split("/");
-  const endPart = splittedUrl[3];
+  const endPart = splittedUrl[splittedUrl.length - 1];
 
   return window.location.replace(`${redirectUrl + endPart}/`);
 }
